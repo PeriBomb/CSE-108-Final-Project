3b88f39036c548c763e77538835ce66df2502bd5
PeriBomb <25spaulding@gmail.com>
2026-05-11 20:32:49 -0700

    Very hopeful fix

--- app.py
+++ app.py
@@ -728,4 +728,4 @@ with app.app_context():
 
 # Run the Flask server
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=False)
